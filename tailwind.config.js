2bced35af93d55de22d23d0639fc8217357fa6ac
anksuunamun <shishkina_veronika_10@mail.ru>
2021-04-23 19:44:16 +0300

    style: add custom colors for bg and text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: {content: ['./public/**/*.html', './src/**/*.vue']},
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+            padding: '2rem'
+        },
+        textColor: {
+            'yellow': '#fae760',
+            'bright-blue': '#60c4fa',
+            'dirty-blue': '#4389af',
+            'bright-pink': '#fa60ad',
+            'dirty-pink': '#af4379',
+            'white': '#fff'
+        },
+        backgroundColor: {
+            'yellow': '#fae760',
+            'bright-blue': '#60c4fa',
+            'dirty-blue': '#4389af',
+            'bright-pink': '#fa60ad',
+            'dirty-pink': '#af4379',
+            'white': '#fff'
+        }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
